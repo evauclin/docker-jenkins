f717effc3b25e3217426c2c7006db3e1386958c8
evauclin <vauclinetienne@gmail.com>
2024-07-29 20:01:19 +0200

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-ressource "aws_instance" "example" {
+resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   tags = {
